48390ceba75cfc11ddb141128e93ec2a5e533758
Katarinchik <katyushabryzgalova@gmail.com>
2023-10-09 13:23:27 +0300

    lab6

---
+++ Lab6_KG/Lab6_KG/Lab6_KG.cpp
@@ -0,0 +1,205 @@
+﻿#include <cstdlib> 
+#include <cstdio> 
+#include <cmath> 
+#include <fstream> 
+#include <vector> 
+#include <iostream> 
+#include <cassert> 
+#include <algorithm>
+#include <string>
+
+template<typename T>
+class Vec3
+{
+public:
+	T x, y, z;
+	Vec3() : x(T(0)), y(T(0)), z(T(0)) {}
+	Vec3(T xx) : x(xx), y(xx), z(xx) {}
+	Vec3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
+	Vec3& normalize()
+	{
+		T nor2 = length2();
+		if (nor2 > 0) {
+			T invNor = 1 / sqrt(nor2);
+			x *= invNor, y *= invNor, z *= invNor;
+		}
+		return *this;
+	}
+	Vec3<T> operator * (const T& f) const { return Vec3<T>(x * f, y * f, z * f); }
+	Vec3<T> operator * (const Vec3<T>& v) const { return Vec3<T>(x * v.x, y * v.y, z * v.z); }
+	T dot(const Vec3<T>& v) const { return x * v.x + y * v.y + z * v.z; }
+	Vec3<T> operator - (const Vec3<T>& v) const { return Vec3<T>(x - v.x, y - v.y, z - v.z); }
+	Vec3<T> operator + (const Vec3<T>& v) const { return Vec3<T>(x + v.x, y + v.y, z + v.z); }
+	Vec3<T>& operator += (const Vec3<T>& v) { x += v.x, y += v.y, z += v.z; return *this; }
+	Vec3<T>& operator *= (const Vec3<T>& v) { x *= v.x, y *= v.y, z *= v.z; return *this; }
+	Vec3<T> operator - () const { return Vec3<T>(-x, -y, -z); }
+	T length2() const { return x * x + y * y + z * z; }
+	T length() const { return sqrt(length2()); }
+	friend std::ostream& operator << (std::ostream& os, const Vec3<T>& v)
+	{
+		os << "[" << v.x << " " << v.y << " " << v.z << "]";
+		return os;
+	}
+};
+
+typedef Vec3<float> Vec3f;
+
+class Shape
+{
+public:
+	Vec3f center;
+	float radius, radius2;
+	Vec3f surfaceColor, emissionColor;
+	float transparency, reflection;
+	Shape(
+		const Vec3f& c,
+		const float& r,
+		const Vec3f& sc,
+		const float& refl = 0,
+		const float& transp = 0,
+		const Vec3f& ec = 0) :
+		center(c), radius(r), radius2(r* r), surfaceColor(sc), emissionColor(ec),
+		transparency(transp), reflection(refl)
+	{
+	}
+
+	bool intersect(const Vec3f& rayorig, const Vec3f& raydir, float& t0, float& t1) const
+	{
+		Vec3f l = center - rayorig;
+		float tca = l.dot(raydir);
+		if (tca < 0) return false;
+		float d2 = l.dot(l) - tca * tca;
+		if (d2 > radius2) return false;
+		float thc = sqrt(radius2 - d2);
+		t0 = tca - thc;
+		t1 = tca + thc;
+
+		return true;
+	}
+};
+
+#define MAX_RAY_DEPTH 5 
+
+float mix(const float& a, const float& b, const float& mix)
+{
+	return b * mix + a * (1 - mix);
+}
+
+Vec3f trace(
+	const Vec3f& rayorig,
+	const Vec3f& raydir,
+	const std::vector<Shape>& spheres,
+	const int& depth)
+{
+	float tnear = 1e8;
+	const Shape* sphere = NULL;
+
+	for (unsigned i = 0; i < spheres.size(); ++i) {
+		float t0 = INFINITY, t1 = INFINITY;
+		if (spheres[i].intersect(rayorig, raydir, t0, t1)) {// пересекает ли этот луч какую-либо геометрию в сцене,вычисляем точку пересечения
+			if (t0 < 0) t0 = t1;
+			if (t0 < tnear) {
+				tnear = t0;
+				sphere = &spheres[i]; 
+			}
+		}
+	}
+	
+	if (!sphere) return Vec3f(2);//нет пересечения - возвращаем цвет фона
+	Vec3f surfaceColor = 0;
+	Vec3f phit = rayorig + raydir * tnear;
+	Vec3f nhit = phit - sphere->center;
+	nhit.normalize();
+	double bias = 1e-4;
+	bool inside = false;
+	if (raydir.dot(nhit) > 0) nhit = -nhit, inside = true;
+	if ((sphere->transparency > 0 || sphere->reflection > 0) && depth < MAX_RAY_DEPTH) {
+		float facingratio = -raydir.dot(nhit);
+		float fresneleffect = mix(pow(1 - facingratio, 3), 1, 0.1);
+		Vec3f refldir = raydir - nhit * 2 * raydir.dot(nhit);
+		refldir.normalize();
+		Vec3f reflection = trace(phit + nhit * bias, refldir, spheres, depth + 1);
+		Vec3f refraction = 0;
+		if (sphere->transparency) {
+			double ior = 1.1, eta = (inside) ? ior : 1 / ior;
+			float cosi = -nhit.dot(raydir);
+			float k = 1 - eta * eta * (1 - cosi * cosi);
+			Vec3f refrdir = raydir * eta + nhit * (eta * cosi - sqrt(k));
+			refrdir.normalize();
+			refraction = trace(phit - nhit * bias, refrdir, spheres, depth + 1);
+		}
+		surfaceColor = (
+			reflection * fresneleffect +
+			refraction * (1 - fresneleffect) * sphere->transparency) * sphere->surfaceColor;
+	}
+	else {
+		for (unsigned i = 0; i < spheres.size(); ++i) {
+			if (spheres[i].emissionColor.x > 0) {
+
+				Vec3f transmission = 1;
+				Vec3f lightDirection = spheres[i].center - phit;
+				lightDirection.normalize();
+				for (unsigned j = 0; j < spheres.size(); ++j) {
+					if (i != j) {
+						float t0, t1;
+						if (spheres[j].intersect(phit + nhit * bias, lightDirection, t0, t1)) {
+							transmission = 0;
+							break;
+						}
+					}
+				}
+				surfaceColor += sphere->surfaceColor * transmission *
+					std::max(float(0), nhit.dot(lightDirection)) * spheres[i].emissionColor;
+			}
+		}
+	}
+
+	return surfaceColor + sphere->emissionColor;//возвращаем цвет луча
+}
+
+
+void render(const std::vector<Shape>& spheres)
+{
+	unsigned width = 1024, height = 768;
+	Vec3f* image = new Vec3f[width * height], * pixel = image;
+	float invWidth = 1 / float(width), invHeight = 1 / float(height);
+	float fov = 70, aspectratio = width / float(height);
+	float angle = tan(3.14 * 0.5 * fov / 180.);
+	for (unsigned y = 0; y < height; ++y) {
+		for (unsigned x = 0; x < width; ++x, ++pixel) {
+			float xx = (2 * ((x + 0.5) * invWidth) - 1) * angle * aspectratio;
+			float yy = (1 - 2 * ((y + 0.5) * invHeight)) * angle;
+			Vec3f raydir(xx, yy, -1);
+			raydir.normalize();
+			*pixel = trace(Vec3f(0), raydir, spheres, 0);
+		}
+	}
+
+	std::ofstream ofs("./trac.ppm", std::ios::out | std::ios::binary);
+	ofs << "P6\n" << width << " " << height << "\n255\n";
+	for (unsigned i = 0; i < width * height; ++i) {
+		ofs << (unsigned char)(std::min(float(1), image[i].x) * 255) <<
+			(unsigned char)(std::min(float(1), image[i].y) * 255) <<
+			(unsigned char)(std::min(float(1), image[i].z) * 255);
+	}
+	ofs.close();
+	delete[] image;
+}
+
+
+int main(int argc, char** argv)
+{
+	srand(13);
+	std::vector<Shape> picture;
+
+	picture.push_back(Shape(Vec3f(0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0));
+	picture.push_back(Shape(Vec3f(-2.0, 0, -16), 2, Vec3f(0.30, 0.30, 1.00), 1, 1.5));
+	picture.push_back(Shape(Vec3f(1.5, -0.5, -18), 2, Vec3f(0.00, 1.00, 0.00), 1, 0.0));
+	picture.push_back(Shape(Vec3f(7, 5, -18), 3, Vec3f(1.00, 0.10, 0.10), 1, 0.0));
+	picture.push_back(Shape(Vec3f(-1.0, -1.5, -12), 1, Vec3f(1.00, 0.00, 1.00), 1, 0.0));
+	picture.push_back(Shape(Vec3f(0.0, 20, -30), 3, Vec3f(0.00, 0.00, 0.00), 0, 0.0, Vec3f(3)));
+	render(picture);
+
+	//system("pause");
+	return 0;
+}
